482db9b5e0f7f4e5a2d785edbfc019f767d49b67
Yacine Rezgui <rezgui.y@gmail.com>
2025-12-15 10:50:42 +0100

    Initialize StaticToggle MacOS app project with initial files and configurations

---
+++ mac/StaticToggle/StaticToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticToggleApp.swift
+//  StaticToggle
+//
+//  Created by Yacine Rezgui on 15.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct StaticToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
